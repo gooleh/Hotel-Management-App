5f088a8a09c9b7ab01c0fc836c23567bf3a02569
landrover1221 <127188503+gooleh@users.noreply.github.com>
2024-05-14 10:06:21 +0900

    Initial Commit

---
+++ INHA_HOTEL_APP_TAE/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  INHA_HOTEL_APP_TAE
+//
+//  Created by 이태규 on 5/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ INHA_HOTEL_APP_TAE/INHA_HOTEL_APP_TAEApp.swift
@@ -0,0 +1,17 @@
+//
+//  INHA_HOTEL_APP_TAEApp.swift
+//  INHA_HOTEL_APP_TAE
+//
+//  Created by 이태규 on 5/14/24.
+//
+
+import SwiftUI
+
+@main
+struct INHA_HOTEL_APP_TAEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
